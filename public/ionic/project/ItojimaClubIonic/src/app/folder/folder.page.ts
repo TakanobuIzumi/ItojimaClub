99120780c48dc35ced1e8bf5ca4e691c419d809a
Takanobu Izumi <snowprint123@gmail.com>
2020-06-24 23:06:25 +0900

    とりあえずコミット

--- public/ionic/project/ItojimaClubIonic/src/app/folder/folder.page.ts
+++ public/ionic/project/ItojimaClubIonic/src/app/folder/folder.page.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-@Component({
+@Component
+({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
-export class FolderPage implements OnInit {
+
+export class FolderPage implements OnInit
+{
   public folder: string;
+  tasks: { name: string }[] = [
+    { name: 'タスク3' },
+    { name: 'タスク4' },
+  ];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute)
+  {
 
-  ngOnInit() {
+  }
+
+  ngOnInit()
+  {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
