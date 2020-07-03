a786f63514b34249e7459a92a389006bf4e1336c
TakanobuIzumi <snowprint123@gmail.com>
2020-07-04 00:20:03 +0900

    minimum project
    minimum project

---
+++ private/izumi/Linux/CUDA_ROS2/cmake-build-debug/CMakeFiles/CUDA_ROS2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CUDA_ROS2.dir/cmake_device_link.o"
+  "CMakeFiles/CUDA_ROS2.dir/main.cu.o"
+  "CUDA_ROS2"
+  "CUDA_ROS2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/CUDA_ROS2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
